831de72bdfb58fb3f89d27484bccd3b68d045e01
poweruser <C:\Users\1\AppData\Roaming\The Bat!>
2021-04-20 13:30:03 +0300

    added some components for client

--- client/pages/tracks/index.tsx
+++ client/pages/tracks/index.tsx
@@ -1,7 +1,16 @@
 import MainLayout from "../../layouts/MainLayout";
 import { Box, Button, Card, Grid } from "@material-ui/core";
+import { useRouter } from "next/router"
+import { ITrack } from "../../types/track";
+import TrackList from "../../components/TrackList";
 
 const Index = () => {
+    const router = useRouter();
+    const tracks: ITrack[] = [
+        { _id: "23123123123", name: "track 1", text: "text song 1", artist: "artist 1", listens: 0, picture: "pic1.jpg", audio: "song1.mp3", comments: [] },
+        { _id: "23123123124", name: "track 2", text: "text song 2", artist: "artist 2", listens: 0, picture: "pic2.jpg", audio: "song2.mp3", comments: [] },
+        { _id: "23123123125", name: "track 3", text: "text song 3", artist: "artist 3", listens: 0, picture: "pic3.jpg", audio: "song3.mp3", comments: [] },
+    ];
     return (
         <MainLayout>
             <Grid container justifyContent='center'>
@@ -14,7 +23,7 @@ const Index = () => {
                             </Button>
                         </Grid>
                     </Box>
-
+                    <TrackList tracks={tracks} />
                 </Card>
             </Grid>
         </MainLayout>
